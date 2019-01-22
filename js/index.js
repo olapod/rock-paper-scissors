3ce08e325661d95c2df9940ffa2abe12490377ce
Ola Podsiadlik <alecs@op.pl>
2019-01-22 22:08:04 +0100

    Final version

--- js/index.js
+++ js/index.js
@@ -1,78 +1,95 @@
 'use strict';
+
 var params = {
   output: document.getElementById('output'),
   result: document.getElementById('result'),
-  addScoreHuman: 0,
-  addScorePlayer: 0,
+  scoreHuman: 0,
+  scoreAI: 0,
   roundResult: document.getElementById('round-score'),
   newGame: document.getElementById('newgame-button'),
   numberGameText: document.getElementById('number-game'),
   progress: [],
   clicksNumber: 0,
+  endGame: null
   }
 
 
 
 //Funkcja ruchu komputera
 
-function playerMoveResult() {
+function getAIscore() {
   
-  var playerScore = Math.floor(Math.random() * 3 + 1);
+  var AIscore = Math.floor(Math.random() * 3 + 1);
   
-  return playerScore;
+  return AIscore;
  };
 
-//Funkcja zamiana wyniku na tekst
-function moveToText(humanMove) {
+//Funkcja zamiany wyniku na tekst
+function changeMoveToText(humanMove) {
   var moveText = ' ';
   
-  if (humanMove === 1) {moveText = 'Papier';}
-  else if (humanMove === 2) {moveText = 'Kamień';}
-  else {moveText = 'Nożyce';}
+  if (humanMove === 1) 
+  {moveText = 'Papier';}
+  else if (humanMove === 2) 
+  {moveText = 'Kamień';}
+  else 
+  {moveText = 'Nożyce';}
   
   return moveText;
 };
 
-//funkcja przycisków
-function compareScore(humanMove, playerMove) {
+//funkcja porównania wyników
+function compareScore(humanMove, AImove) {
   
   var scoreText = ' ';
   
-  if (humanMove === playerMove) {scoreText = 'REMIS!';}
+  if (humanMove === AImove) 
+  {scoreText = 'REMIS!';}
+  
   else if(
-    (humanMove === 1 && playerMove === 2) ||
-    (humanMove === 2 && playerMove === 3) ||
-    (humanMove === 3 && playerMove === 1)
-  ) {scoreText = 'WYGRAŁEŚ!';}
-  else {scoreText = 'PRZEGRAŁEŚ!';}
+    (humanMove === 1 && AImove === 2) ||
+    (humanMove === 2 && AImove === 3) ||
+    (humanMove === 3 && AImove === 1)) 
+  {scoreText = 'WYGRAŁEŚ!';}
+  
+  else 
+  {scoreText = 'PRZEGRAŁEŚ!';}
   
   return scoreText;
-  };
+  
+};
 
-//funkcja blokowania klawiszy
 
-document.getElementById('paper-button').disabled = true;
-document.getElementById('stone-button').disabled = true;
-document.getElementById('shears-button').disabled = true;
+//blokowanie klawiszy przed wyborem ilości rund
 
+var elems = document.getElementsByClassName('player-move');
+
+for(var i = 0; i < elems.length; i++) {
+  elems[i].disabled = true;
+  }
 
 
 //funkcja wyświetlania wyniku pojedynczej gry
-function gameScoreText (humanText, playerText, compareResult) { 
-params.output.innerHTML = 'wybór gracza: ' + humanText +  '<br><br>' + 'wybór komputera: ' + playerText + '<br><br>' + 'Wynik pojedynku: ' + compareResult;};
 
-//funkcje zliczania wyników i wyświetlania wyników
+function displayGameScoreText (humanText, AItext, compareResult) { 
+
+params.output.innerHTML = 'wybór gracza: ' + humanText +  '<br><br>' + 'wybór komputera: ' + AItext + '<br><br>' + 'Wynik pojedynku: ' + compareResult;
+};
+
+//funkcje zliczanania wyników i wyświetlannia wyników
 
 
 
-function humanSummary (scoreText) {
-  if (scoreText === 'WYGRAŁEŚ!') {params.addScoreHuman += 1;}};
+function getHumanResult (scoreText) {
+  if (scoreText === 'WYGRAŁEŚ!') {params.scoreHuman += 1;}
+};
 
-function playerSummary (scoreText) {
-  if (scoreText === 'PRZEGRAŁEŚ!') {params.addScorePlayer += 1;}};
+function getAIresult (scoreText) {
+  if (scoreText === 'PRZEGRAŁEŚ!') {params.scoreAI += 1;}
+};
   
-function displayResults(addScoreHuman, addScorePlayer) {
-params.result.innerHTML = 'Wyniki rundy' + '<br><br>' + 'Wygrane gracza: ' + params.addScoreHuman +  '<br><br>' + 'Wygrane komputera: ' + params.addScorePlayer;
+function displayResults() {
+params.result.innerHTML = 'Wyniki rundy' + '<br><br>' + 'Wygrane gracza: ' + params.scoreHuman +  '<br><br>' + 'Wygrane komputera: ' + params.scoreAI;
 };
 
  // licznik gier  
@@ -91,71 +108,88 @@ params.newGame.addEventListener('click', function(){
 	var numberGameChoice = window.prompt('Do ilu wygranych gramy w jednej rundzie?');
   
   if ((!isNaN(numberGameChoice)) 
-      && (numberGameChoice != 0))  {
+      && (numberGameChoice != 0)) {
+
   numberGame = parseInt(numberGameChoice);
     
   params.numberGameText.innerHTML = 'Gramy do ' + numberGame + ' wygranych';
+  
+  var elems = document.getElementsByClassName('player-move');
+  
+  for(var i = 0; i < elems.length; i++) {
+          elems[i].disabled = false;
+          }
+  document.getElementById('newgame-button').disabled = true;
+    }
+  
+    else 
     
-  document.getElementById('paper-button').disabled = false;
-  document.getElementById('stone-button').disabled = false;
-  document.getElementById('shears-button').disabled = false;}
-else {params.numberGameText.innerHTML = 'Podaj LICZBĘ rund! '};
+    {params.numberGameText.innerHTML = 'Podaj LICZBĘ rund! '
+    };
   
   return numberGame;
 });
 
 //funkcje kończenia rundy
-var endGame;
 
 function finishRound(){
- if (
-      (numberGame === params.addScoreHuman) ||
-      (numberGame === params.addScorePlayer))
-     {endGame = true;
-      document.getElementById('paper-button').disabled = true;
-      document.getElementById('stone-button').disabled = true;
-      document.getElementById('shears-button').disabled = true;
+ if ((numberGame === params.scoreHuman) ||
+      (numberGame === params.scoreAI))
+     
+      {params.endGame = true;
+      
+      document.getElementById('newgame-button').disabled = false;
+
+      var elems = document.getElementsByClassName('player-move');
+        for(var i = 0; i < elems.length; i++) {
+          elems[i].disabled = true;
+          }
+      
       params.result.innerHTML = '';
       params.output.innerHTML = '';
-      
       }
- else {endGame = false;}
-  
-   };
+
+      else {
+        params.endGame = false;
+      }
+};
 
 function theEnd(){
   
-if (endGame == true && numberGame === params.addScoreHuman) {
+if (params.endGame == true && numberGame === params.scoreHuman) {
     
   showSummary();
     
-    params.roundResult.innerHTML = 'WYGRAŁEŚ RUNDĘ! :(' + '<br>' + 'Wygrane gracza: ' + params.addScoreHuman + '  Wygrane komputera: ' + params.addScorePlayer;
+  params.roundResult.innerHTML = 'WYGRAŁEŚ RUNDĘ! ' + '<br><i class="fas fa-grin-alt smile"></i><br>' + ' Wygrane gracza: ' + params.scoreHuman + '  Wygrane komputera: ' + params.scoreAI;
     
   }
   
-  else if (endGame == true && numberGame === params.addScorePlayer) {
+  else if (params.endGame == true && numberGame === params.scoreAI) {
    
     showSummary();
-    params.roundResult.innerHTML = 'PRZEGRAŁEŚ RUNDĘ! :(' + '<br>' + 'Wygrane gracza: ' + params.addScoreHuman + '  Wygrane komputera: ' + params.addScorePlayer;
+    params.roundResult.innerHTML = 'PRZEGRAŁEŚ RUNDĘ! ' + '<br><i class="fas fa-frown sad"></i><br>' + ' Wygrane gracza: ' + params.scoreHuman + '  Wygrane komputera: ' + params.scoreAI;
    
-   console.log(params.addScoreHuman, params.addScorePlayer)
-  }};
+  }
+};
 
 
   function showSummary() {
     showModal(event);
    
     for (var i = 0; i < params.progress.length; i++) {
-      var node = document.createElement("tr")
-      for (var key of ['gameNo', 'human', 'player', 'gameScore']) {
-        var tb = document.createElement("td")
-        tb.innerHTML = params.progress[i][key]
-        node.appendChild(tb)
+      var node = document.createElement('tr')
+
+      for (var key of ['gameNo', 'human', 'AI', 'gameScore']) {
+          var tb = document.createElement('td')
+          tb.innerHTML = params.progress[i][key]
+          node.appendChild(tb)
       }
-      document.getElementById("my-data").appendChild(node);
+      
+      document.getElementById('my-data').appendChild(node);
+
     }
     params.numberGameText.innerHTML = 'NACIŚNIJ PRZYCISK NOWA RUNDA';
-  }
+};
 
     //Funkcja wyniku człowieka
 
@@ -167,92 +201,92 @@ var buttonsList = document.getElementsByClassName('player-move');
 
 for (var i=0; i < buttonsList.length; i++) {
   initClickWatch(i);
-  }
+}
 
-
-  function initClickWatch(value) {
+function initClickWatch(value) {
   
-    buttonsList[value].addEventListener('click', function() {
+  buttonsList[value].addEventListener('click', function() {
     var humanMoveAttribute = buttonsList[value].getAttribute('data-move');
     var humanMove = Number(humanMoveAttribute);
   
-    var humanText = moveToText(humanMove);
-    var playerMove = playerMoveResult();
-    var playerText = moveToText(playerMove);
-    var compareResult = compareScore(humanMove, playerMove);
+    var humanText = changeMoveToText(humanMove);
+    var AImove = getAIscore();
+    var AItext = changeMoveToText(AImove);
+    var compareResult = compareScore(humanMove, AImove);
     var game = {
       gameNo: params.clicksNumber,
       human: humanText,
-      player: playerText,
+      AI: AItext,
       gameScore: compareResult,
-     }
-    
-    params.progress.push(game);
-    
-
-    compareScore(humanMove, playerMove);
-    gameScoreText (humanText, playerText, compareResult);
-    humanSummary(compareResult);
-    playerSummary(compareResult);
-    displayResults(params.addScoreHuman, params.addScorePlayer);
-    finishRound();
-    theEnd();
-
-      
-    
-       });        
       }
+    
+  params.progress.push(game);
+    
+
+  compareScore(humanMove, AImove);
+  displayGameScoreText (humanText, AItext, compareResult);
+  getHumanResult(compareResult);
+  getAIresult(compareResult);
+  displayResults(params.scoreHuman, params.scoreAI);
+  finishRound();
+  theEnd();
+
+  });        
+}
       
           
     
-    //Funkcja wyświetlania modala
+    //Funkcja wyświetlannia modala
 
-      function showModal(event){
-        event.preventDefault();
-        
-      var modals =  document.querySelectorAll('.modal');
-        
-      for(var i = 0; i < modals.length; i++){
-      modals[i].classList.remove('show');}
-        
-      document.querySelector('#modal-one').classList.add('show');
-       
-      document.querySelector('#modal-overlay').classList.add('show');
-          
-      };
-      
-      
-    
-      var hideModal = function(event){
-        event.preventDefault();
-        document.querySelector('#modal-overlay').classList.remove('show');
-        params.progress.length = 0; 
-        var table = document.getElementById('my-data');
-        table.innerHTML = '';
-        params.clicksNumber = 0;
-        params.addScoreHuman = 0;
-        params.addScorePlayer = 0;
-      };
-      
-      var closeButtons = document.querySelectorAll('.modal .close');
-      
-      for(var i = 0; i < closeButtons.length; i++){
-        closeButtons[i].addEventListener('click', hideModal);
-      }
-      
-      // Dobrą praktyką jest również umożliwianie zamykania modala poprzez kliknięcie w overlay. 
-      
-      document.querySelector('#modal-overlay').addEventListener('click', hideModal);
-      
-      // Musimy jednak pamiętać, aby zablokować propagację kliknięć z samego modala - inaczej każde kliknięcie wewnątrz modala również zamykałoby go. 
-      
-      var modals = document.querySelectorAll('.modal');
-      
-      for(var i = 0; i < modals.length; i++){
-        modals[i].addEventListener('click', function(event){
-          event.stopPropagation();
-        });
-      }
-    
-     
+function showModal(event){
   
+  event.preventDefault();
+        
+  var modals =  document.querySelectorAll('.modal');
+        
+  for(var i = 0; i < modals.length; i++){
+      modals[i].classList.remove('show');
+    }
+        
+  document.querySelector('#modal-one').classList.add('show');
+       
+  document.querySelector('#modal-overlay').classList.add('show');
+        
+};
+      
+   //Funkcja zamykania modala   
+    
+var hideModal = function(event){
+  
+  event.preventDefault();
+  document.querySelector('#modal-overlay').classList.remove('show');
+  params.progress.length = 0; 
+  var table = document.getElementById('my-data');
+  table.innerHTML = '';
+  params.clicksNumber = 0;
+  params.scoreHuman = 0;
+  params.scoreAI = 0;
+};
+      
+var closeButtons = document.querySelectorAll('.modal .close');
+      
+for(var i = 0; i < closeButtons.length; i++){
+
+    closeButtons[i].addEventListener('click', hideModal);
+};
+       
+      
+document.querySelector('#modal-overlay').addEventListener('click', hideModal);
+      
+      // Blokada kliknięć z samego modala
+      
+var modals = document.querySelectorAll('.modal');
+      
+for(var i = 0; i < modals.length; i++){
+
+  modals[i].addEventListener('click', function(event){
+    event.stopPropagation();
+  });
+};
+    
+ 
